674b497bae682c8e550d885ee7cadcb3d5e45371
Samuel Williams <samuelhwilliams@gmail.com>
2024-02-18 21:15:57 +0000

    Update the colour scheme to catppuccin macchiato

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
   {
     "catppuccin/nvim",
-    lazy = false,
+    lazy = true,
     name = "catppuccin",
     -- you can do it like this with a config function
     -- or just use opts table
     opts = {
-      flavour = "mocha",
+      flavour = "macchiato",
     },
   },
   {
