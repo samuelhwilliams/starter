df226b7081e9eddbdedf46e7446aa59e7ff7a5c2
Samuel Williams <samuelhwilliams@gmail.com>
2024-02-18 21:17:27 +0000

    Misc other, broken keymaps?

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local opts = { noremap = true, silent = true }
+
+-- These aren't working for some reason?
+-- vim.keymap.set("n", "+", "<C-a>")
+-- vim.keymap.set("n", "-", "<C-x>")
