8ee7e5c921f74e9c71bf2c7d708856736ae57f8d
Samuel Williams <samuelhwilliams@gmail.com>
2024-03-22 09:20:08 +0000

    Switch back to hesh7th nvim-cmp after emojis merged

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,6 +1,6 @@
 return {
   "hrsh7th/nvim-cmp",
-  dependencies = { "samuelhwilliams/cmp-emoji" },
+  dependencies = { "hrsh7th/cmp-emoji" },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     table.insert(opts.sources, { name = "emoji" })
